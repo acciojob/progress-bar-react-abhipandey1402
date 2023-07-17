3dce6bc8394590e5777589dce8dfb8f03aafdceb
Abhi Pandey <pandeyabhi142002@gmail.com>
2023-07-17 17:54:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 
-import React from "react";
 import './../styles/App.css';
+import React, { useEffect, useState } from 'react'
+
 
 const App = () => {
+
+  const [progress, setProgress] = useState(0);
+
+    useEffect(()=> {
+        let intervalId = setInterval(()=> {
+            setProgress(prev => prev+10)
+        }, 1000); 
+
+        return () => {
+            clearTimeout(intervalId);
+        }
+    }, []);
+
+    if(progress > 100){
+        setProgress(0)
+    }
+  
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{width: '400px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <h3>Progress Bar</h3>
+        <div id='barOuter' style={{width: '300px', height: '15px', backgroundColor: 'grey'}}>
+            <div id='barInner' style={{width: `${progress}%`, height: '100%', backgroundColor: 'blue'}}></div>
+        </div>
+        <p>{progress}%</p>
     </div>
   )
 }
